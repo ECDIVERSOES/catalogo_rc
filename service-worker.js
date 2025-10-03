4a09ad0d43fe05466e00d81b353a9d58b7e6bbb0
ECDIVERSOES <esiocosta17@gmail.com>
2025-10-02 21:16:09 -0300

    Atualização da versão do catálogo no service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'karaoke-cache-v01'; // Altere a versão para forçar atualização
+const CACHE_NAME = 'karaoke-cache-v02'; // Altere a versão para forçar atualização
 const ASSETS = [
   './',
   './index.html',
